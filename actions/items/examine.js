109944e4b8bdbf71603809b6dc9a83d5fec8ccf6
Frank Stokes <frank@wearereasonablepeople.com>
2017-11-02 08:31:04 +0100

    Hook Examine events

--- actions/items/examine.js
+++ actions/items/examine.js
@@ -8,8 +8,12 @@ module.exports = async (state, world, input, output) => {
   const items = getItemPromptForRoomAndInventory(state, world);
   if (items.length) {
     const chosenItem = await input.choice(WHICH_ITEM, items);
-    const description = await util.getDynamicProperty(state, world, world.items[chosenItem], 'description');
+    const worldItem = world.items[chosenItem];
+
+    if (!util.hookEvent(worldItem, 'onBeforeExamine', state, world)) return;
+    const description = await util.getDynamicProperty(state, world, worldItem, 'description');
     output.writeLine(description);
+    if (!util.hookEvent(worldItem, 'onAfterExamine', state, world)) return;
   } else {
     output.writeLine(NOTHING_TO_EXAMINE);
   }
