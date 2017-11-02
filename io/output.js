4b3d412d699f409d19cb554dc3fc3e3de7276eef
Frank Stokes <frank@wearereasonablepeople.com>
2017-11-02 19:52:51 +0100

    Adds a line pattern function to output

--- io/output.js
+++ io/output.js
@@ -2,5 +2,6 @@ module.exports = {
   write: (str) => process.stdout.write(str),
   writeLine: (str) => process.stdout.write(`${str}\n`),
   newLine: () => process.stdout.write('\n'),
+  line: (pattern, times) => Array.from(Array(times)).fill(pattern).join(''),
   clear: () => process.stdout.write('\x1b[2J\x1b[1;1H')
 };
