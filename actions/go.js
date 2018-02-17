49e120f04430a38ee5f9569d5dc1efba0b3afda2
Frank Stokes <frank@wearereasonablepeople.com>
2018-02-17 15:52:10 +0100

    Refactors go

--- actions/go.js
+++ actions/go.js
@@ -1,19 +1,47 @@
-const { getDynamicProperty } = require('../util');
+const util = require('../util');
+const {
+  maybeFromCondition,
+  maybeContinue,
+  propDefault,
+  mutableSet,
+  pipePromise
+} = util.functional;
+const {
+  curry,
+  pipe,
+  pipeP,
+  prop,
+  length,
+  compose,
+  gt,
+  filter,
+  __,
+  map,
+  append,
+  equals,
+  not,
+  ifElse,
+  find
+} = require('ramda');
+
 const enterRoom = require('./enter-room');
 const {
   OPTION_STAY,
   YOU_CAN_GO
 } = require('../strings.js');
 
-module.exports = async (state, world, input, output) => {
-  const currentLocation = world.rooms[state.location];
-  const exits = await getDynamicProperty(state, world, currentLocation, 'exits');
-  const chosenExit = await input.choice(YOU_CAN_GO, [...exits, OPTION_STAY]);
-
-  if (chosenExit !== OPTION_STAY) {
-    const doEnter = ('onExit' in currentLocation)
-      ? await currentLocation.onExit(state, world)
-      : true;
-    if (doEnter) await enterRoom(state, world, chosenExit, input, output);
-  }
+module.exports = (state, world, input, output) => {
+  return pipeP(
+    util.getDynamicPropertyFp(state, world, 'exits'),            // [exit]
+    append(OPTION_STAY),                                    // [exit]
+    input.choice(YOU_CAN_GO),                               // chosenExit
+    maybeFromCondition(compose(not, equals(OPTION_STAY))),  // Maybe chosenExit
+    maybeContinue(chosenExit => pipePromise(
+      () => world.rooms[state.location],                    // currentLocation
+      util.hookEventFp('onExit', state, world),             // currentLocation
+      maybeContinue(pipeP(
+        () => enterRoom(state, world, chosenExit, input, output)
+      ))
+    )(chosenExit))
+  )(world.rooms[state.location]);
 };
